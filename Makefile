a22cecc21fd78ff06a1d21e0af706462e42a31fb
Sylvain Nicolet <nicoletsylvain@gmail.com>
2022-10-13 19:13:00 +0200

    Add calloc, strdup

--- Makefile
+++ Makefile
@@ -31,7 +31,9 @@ SRCS	= 	ft_isalpha.c \
 			ft_strncmp.c \
 			ft_memchr.c \
 			ft_memcmp.c \
-			ft_atoi.c
+			ft_atoi.c \
+			ft_calloc.c \
+			ft_strdup.c
 
 OBJS	= ${SRCS:.c=.o}
 
--- libft.h
+++ libft.h
@@ -36,5 +36,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *str);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 #endif
