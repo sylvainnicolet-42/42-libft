0a23f817a6a53723b4a49a1894504dbf2d5b283b
Sylvain Nicolet <nicoletsylvain@gmail.com>
2022-10-14 21:39:25 +0200

    Add strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,7 +12,30 @@
 
 #include "libft.h"
 
-char 	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
+	char	*ptr;
+	size_t	length;
+	size_t	i;
+	size_t	j;
 
+	length = ft_strlen(s1) + ft_strlen(s2);
+	ptr = malloc(sizeof(char) * (length + 1));
+	if (!ptr)
+		return (0);
+	i = 0;
+	while (i < (size_t) ft_strlen(s1))
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < (size_t) ft_strlen(s2))
+	{
+		ptr[i] = s2[j];
+		j++;
+		i++;
+	}
+	ptr[i] = 0;
+	return (ptr);
 }
