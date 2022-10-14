8c338451bed0d04df5e207b0f4a06d2ebba080d1
Sylvain Nicolet <nicoletsylvain@gmail.com>
2022-10-14 22:22:28 +0200

    Update strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: synicole <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 21:40:44 by synicole          #+#    #+#             */
+/*   Updated: 2022/10/14 21:40:46 by synicole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*ptr;
+
+	// Calculer la longueur
+
+	ptr = malloc(sizeof(char) * (length + 1));
+	if (!ptr)
+		return (0);
+
+	// Remplir ptr
+
+
+	return (ptr);
+}
