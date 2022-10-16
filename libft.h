27e7ea7d9c52d32526f52767dfcb3855d03a2a1a
Sylvain Nicolet <nicoletsylvain@gmail.com>
2022-10-16 15:04:26 +0200

    Add putnbr_fd

--- libft.h
+++ libft.h
@@ -46,5 +46,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
