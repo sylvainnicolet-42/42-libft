efd7cf8a71b7ce7e41916fa4ed4d01e2c58a3733
Sylvain Nicolet <nicoletsylvain@gmail.com>
2022-10-16 14:44:38 +0200

    Add putchar_fd

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -16,15 +16,26 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*ptr;
 	size_t	length;
+	size_t	i_s1;
 	size_t	i_set;
-	size_t	is_s1;
 
 	length = ft_strlen(s1);
+	i_s1 = 0;
 	i_set = 0;
-	is_s1 = 0;
-	while (set[i_set])
-	{
 
+	// Tant que s1 n'est pas fini et que is_s1 < longueur de set
+	while (s1[i_s1] && i_s1 < (size_t) ft_strlen(set))
+	{
+		i_set = 0;
+		while (set[i_set])
+		{
+			if (s1[i_s1] == set[i_set + i_s1])
+			{
+				printf("%c\n", s1[i_s1]);
+			}
+			i_set++;
+		}
+		i_s1++;
 	}
 	ptr = ft_calloc(sizeof(char), length + 1);
 	if (!ptr)
